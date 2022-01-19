666bb525532e61bf9ecdbbd1eb7ba3c9b6e1364b
dwhenson <dave.henson@gmail.com>
2022-01-19 14:06:45 +0300

    ✅ ADD: small problems - 1 - 3

---
+++ 01-Easy-1/02.js
@@ -0,0 +1,3 @@
+for (let number_ = 1; number_ < 100; number_ += 1) {
+  console.log(number_);
+}
---
+++ 01-Easy-1/03.js
@@ -0,0 +1,5 @@
+for (let number_ = 1; number_ < 100; number_ += 1) {
+  if (!(number_ % 2)) {
+    console.log(number_);
+  }
+}
