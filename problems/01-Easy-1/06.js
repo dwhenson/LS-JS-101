dfbfde1ad07aee253c3abde24cac22a82f2eda15
dwhenson <dave.henson@gmail.com>
2022-01-20 21:25:53 +0300

    ✅ ADD: problem 6 solution

--- problems/01-Easy-1/06.js
+++ problems/01-Easy-1/06.js
@@ -1,20 +1,36 @@
-// const readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
 
-const sum = function (max) {
+const sum = function (maximum) {
   let total = 0;
-  for (let current = 1; current <= max; current += 1) {
+  for (let current = 1; current <= maximum; current += 1) {
     total += current;
   }
   return total;
 };
 
-const product = function (max) {
+const product = function (maximum) {
   let total = 1;
-  for (let current = 1; current <= max; current += 1) {
+  for (let current = 1; current <= maximum; current += 1) {
     total *= current;
   }
   return total;
 };
 
-sum(5); // ?
-product(6); // ?
+const maximum = Number.parseInt(
+  readlineSync.question("Please enter an integer greater than 0:\n")
+);
+if (maximum) {
+  const calculation = readlineSync.keyIn(
+    "Enter 's' to compute the sum, or 'p' to compute the product.\n",
+    { limit: "sp" }
+  );
+  if (calculation === "s") {
+    console.log(`The sum of the integers between 1 and ${maximum} is ${sum(maximum)}.`);
+  } else {
+    console.log(
+      `The product of the integers between 1 and ${maximum} is ${product(maximum)}.`
+    );
+  }
+} else {
+  console.log("Sorry that's not a number");
+}
