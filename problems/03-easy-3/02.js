0d2f61930d82031e05293acbdd040063ee541d8e
dwhenson <dave.henson@gmail.com>
2022-01-21 16:02:45 +0300

    ✅ ADD: prep for easy 3

---
+++ problems/03-easy-3/02.js
@@ -0,0 +1,12 @@
+/* Write a function that will take a short line of text, and write it to the console log within a box.
+
+logInBox('To boldly go where no one has gone before.');
+
+will log on the console:
+
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+*/
