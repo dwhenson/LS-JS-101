a1b1d03317214f27dbb100fbfbded87ee33c4f45
dwhenson <dave.henson@gmail.com>
2022-02-09 16:32:47 +0300

    ✅ ADD: lesson 3 easy 01 solutions

---
+++ lessons/lesson-3/01-easy1/08.js
@@ -0,0 +1,6 @@
+// How can we add the family pet, "Dino", to the following array?
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones = [...flintstones, "Dino"]; //?
+// flintstones.push("Dino");
+// flintstones[flintstones.length] = "Dino";
---
+++ lessons/lesson-3/01-easy1/09.js
@@ -0,0 +1,5 @@
+// How can we add the family pet, "Dino", to the following array?
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones = [...flintstones, "Dino", "Hoppy"];
+// flintstones.push("Dino", "Hoppy");
