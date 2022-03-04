87b8f27ce5d6cf6e5c840283ce8b45efb146813c
dwhenson <dave.henson@gmail.com>
2022-03-04 08:34:15 +0300

    ✅ ADD: medium 02-01 solution

--- problems/10-medium-02/01.js
+++ problems/10-medium-02/01.js
@@ -1,12 +1,32 @@
 function letterPercentages(string) {
-  return string;
+  const percentages = {
+    lowercase: 0,
+    uppercase: 0,
+    neither: 0,
+  };
+
+  for (const letter of string.split("")) {
+    if (!"abcdefghijklmnopqrstuvwxyx".includes(letter.toLowerCase())) {
+      percentages.neither += 1;
+      continue;
+    }
+    letter === letter.toLowerCase()
+      ? (percentages.lowercase += 1)
+      : (percentages.uppercase += 1);
+  }
+
+  for (const item of Object.entries(percentages)) {
+    percentages[item[0]] = ((item[1] / string.length) * 100).toFixed(2);
+  }
+
+  return percentages;
 }
 
-letterPercentages("abCdef 123");
+letterPercentages("abCdef 123"); //?
 // { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
 
-letterPercentages("AbCd +Ef");
-// { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+letterPercentages("AbCd +Ef"); //?
+// // { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
 
-letterPercentages("123");
-// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
+letterPercentages("123"); //?
+// // { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
